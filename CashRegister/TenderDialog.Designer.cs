6e37609c87ab447bc47ac31455c5f6e501cd9b00
James Birdsall <jwbirdsa@picarefy.com>
2015-04-26 12:23:21 -0700

    Enlarge tender dialogs for better viewing

--- CashRegister/TenderDialog.Designer.cs
+++ CashRegister/TenderDialog.Designer.cs
@@ -37,26 +37,29 @@
             // lLabel
             // 
             this.lLabel.AutoSize = true;
+            this.lLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lLabel.Location = new System.Drawing.Point(12, 9);
             this.lLabel.Name = "lLabel";
-            this.lLabel.Size = new System.Drawing.Size(43, 13);
+            this.lLabel.Size = new System.Drawing.Size(88, 26);
             this.lLabel.TabIndex = 0;
             this.lLabel.Text = "Amount";
             // 
             // tData
             // 
-            this.tData.Location = new System.Drawing.Point(61, 6);
+            this.tData.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tData.Location = new System.Drawing.Point(106, 6);
             this.tData.Name = "tData";
-            this.tData.Size = new System.Drawing.Size(118, 20);
+            this.tData.Size = new System.Drawing.Size(149, 32);
             this.tData.TabIndex = 1;
             this.tData.TextChanged += new System.EventHandler(this.tData_TextChanged);
             // 
             // bCancel
             // 
             this.bCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.bCancel.Location = new System.Drawing.Point(104, 35);
+            this.bCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bCancel.Location = new System.Drawing.Point(135, 54);
             this.bCancel.Name = "bCancel";
-            this.bCancel.Size = new System.Drawing.Size(75, 23);
+            this.bCancel.Size = new System.Drawing.Size(92, 47);
             this.bCancel.TabIndex = 8;
             this.bCancel.Text = "Cancel";
             this.bCancel.UseVisualStyleBackColor = true;
@@ -65,9 +68,10 @@
             // 
             this.bOK.DialogResult = System.Windows.Forms.DialogResult.OK;
             this.bOK.Enabled = false;
-            this.bOK.Location = new System.Drawing.Point(14, 35);
+            this.bOK.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bOK.Location = new System.Drawing.Point(38, 54);
             this.bOK.Name = "bOK";
-            this.bOK.Size = new System.Drawing.Size(75, 23);
+            this.bOK.Size = new System.Drawing.Size(91, 47);
             this.bOK.TabIndex = 7;
             this.bOK.Text = "OK";
             this.bOK.UseVisualStyleBackColor = true;
@@ -76,7 +80,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(198, 70);
+            this.ClientSize = new System.Drawing.Size(267, 112);
             this.Controls.Add(this.bCancel);
             this.Controls.Add(this.bOK);
             this.Controls.Add(this.tData);
--- CashRegister/TwoTenderDialog.Designer.cs
+++ CashRegister/TwoTenderDialog.Designer.cs
@@ -39,9 +39,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(22, 38);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 47);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.Size = new System.Drawing.Size(92, 26);
             this.label1.TabIndex = 5;
             this.label1.Text = "Check #";
             // 
@@ -49,9 +50,10 @@
             // 
             this.bOK.DialogResult = System.Windows.Forms.DialogResult.OK;
             this.bOK.Enabled = false;
-            this.bOK.Location = new System.Drawing.Point(16, 65);
+            this.bOK.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bOK.Location = new System.Drawing.Point(31, 91);
             this.bOK.Name = "bOK";
-            this.bOK.Size = new System.Drawing.Size(75, 23);
+            this.bOK.Size = new System.Drawing.Size(95, 37);
             this.bOK.TabIndex = 3;
             this.bOK.Text = "OK";
             this.bOK.UseVisualStyleBackColor = true;
@@ -59,34 +61,38 @@
             // bCancel
             // 
             this.bCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.bCancel.Location = new System.Drawing.Point(111, 65);
+            this.bCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bCancel.Location = new System.Drawing.Point(143, 91);
             this.bCancel.Name = "bCancel";
-            this.bCancel.Size = new System.Drawing.Size(75, 23);
+            this.bCancel.Size = new System.Drawing.Size(95, 37);
             this.bCancel.TabIndex = 4;
             this.bCancel.Text = "Cancel";
             this.bCancel.UseVisualStyleBackColor = true;
             // 
             // tCheckNumber
             // 
-            this.tCheckNumber.Location = new System.Drawing.Point(76, 35);
+            this.tCheckNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tCheckNumber.Location = new System.Drawing.Point(116, 44);
             this.tCheckNumber.Name = "tCheckNumber";
-            this.tCheckNumber.Size = new System.Drawing.Size(100, 20);
+            this.tCheckNumber.Size = new System.Drawing.Size(132, 32);
             this.tCheckNumber.TabIndex = 1;
             // 
             // tAmount
             // 
-            this.tAmount.Location = new System.Drawing.Point(76, 6);
+            this.tAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tAmount.Location = new System.Drawing.Point(116, 6);
             this.tAmount.Name = "tAmount";
-            this.tAmount.Size = new System.Drawing.Size(100, 20);
+            this.tAmount.Size = new System.Drawing.Size(132, 32);
             this.tAmount.TabIndex = 0;
             this.tAmount.TextChanged += new System.EventHandler(this.tAmount_TextChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(22, 9);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.Size = new System.Drawing.Size(88, 26);
             this.label2.TabIndex = 1;
             this.label2.Text = "Amount";
             // 
@@ -94,7 +100,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(198, 103);
+            this.ClientSize = new System.Drawing.Size(280, 145);
             this.Controls.Add(this.tAmount);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.tCheckNumber);
